969f0c11e4c54a71ffe999983bb1efd9bdd160dd
Roberto <robertobrum@yahoo.com.br>
2021-08-19 21:58:15 -0400

    Fim do curso

--- src/main/java/com/github/roberto/citiesapi/cities/CityResource.java
+++ src/main/java/com/github/roberto/citiesapi/cities/CityResource.java
@@ -1,10 +1,15 @@
 package com.github.roberto.citiesapi.cities;
 
+import com.github.roberto.citiesapi.countries.Country;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("cities")
